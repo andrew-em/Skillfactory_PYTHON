cf061f25f105600daaa88cb62aaa20d6a8acf9d8
andrew-em <76231307+andrew-em@users.noreply.github.com>
2022-11-25 17:08:03 +0300

    Update config.py

--- FP_PYTHON(Telegram Bot)/config.py
+++ FP_PYTHON(Telegram Bot)/config.py
@@ -1,7 +1,5 @@
 TOKEN = '5859702860:AAEeZW8DryCEuuaITynDjFg4tDvNacf-GKw'
 
-api_key = '09adff6f47d08c4b8ea448c04e0d4a318c1c84f3f9d23050dd562321a6d4b2ff'
-
 money = {'рубль': 'RUB',
          'доллар': 'USD',
          'евро': 'EUR',
