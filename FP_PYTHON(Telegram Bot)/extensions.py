1d2152d060823008400de301783f6c40bccace0e
andrew-em <76231307+andrew-em@users.noreply.github.com>
2022-11-26 21:53:14 +0300

    Update extensions.py

--- FP_PYTHON(Telegram Bot)/extensions.py
+++ FP_PYTHON(Telegram Bot)/extensions.py
@@ -31,3 +31,5 @@ class CurrencyConverter:
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[money[base]]
+        
+        return total_base * amount
